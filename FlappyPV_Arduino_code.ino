be416f55f80e7feac30a017962e19b4dc5a3b3e1
sebpont <sebpont91@gmail.com>
2017-05-10 10:25:26 +0100

    Add files via upload

---
+++ FlappyPV_Arduino_code.ino
@@ -0,0 +1,16 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  Serial.println("v");
+  int sensorValue0 = analogRead(A0);
+  Serial.println(sensorValue0);
+  int sensorValue1 = analogRead(A1);
+  Serial.println(sensorValue1);
+  int sensorValue2 = analogRead(A2);
+  Serial.println(sensorValue2);
+  delay(50);
+}
